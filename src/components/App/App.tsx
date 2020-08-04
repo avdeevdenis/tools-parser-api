ef2ae5827c839e9ca36e99302c924a5b53bf6ff3
Avdeev Denis <av-denis@yandex-team.ru>
2020-08-04 19:16:45 +0300

    Начало разработки парсера SberMarket'а Ленты

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DrawerComponent from '../Drawer/Drawer.jsx';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <DrawerComponent />
+    </div>
+  );
+};
+
+export default App;
